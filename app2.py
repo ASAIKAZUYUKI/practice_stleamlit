b67c564648bc5ca9157fceeb5641b8b4a1089837
浅井 和之 <asakazuyama2000@gmail.com>
2022-11-01 16:42:59 +0900

    2回目

--- app2.py
+++ app2.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
 import pandas as pd
-df = pd.DataFrame({'col1': [1,2,3]})
-df
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig
